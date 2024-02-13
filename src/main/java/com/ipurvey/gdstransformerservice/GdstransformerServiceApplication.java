6f888653ab1aac620521682b7e0a2a95b85431c4
Nabeel Tariq Bhatti <nabeel.tariq@starzplayarabia.com>
2024-02-13 13:35:03 +0500

    initial commit

---
+++ src/main/java/com/ipurvey/gdstransformerservice/GdstransformerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ipurvey.gdstransformerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdstransformerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdstransformerServiceApplication.class, args);
+	}
+
+}
